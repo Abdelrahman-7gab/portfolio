5a5703aafaa4b8c64ec4acb84ea3ce7cb6354d8e
Abdelrahman Hegab <a.hegab@paywithspare.com>
2025-02-15 17:16:40 +0200

    feat/bouncing animation on hover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, type ElementRef, type OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  @ViewChild('nameText', { static: true }) nameText!: ElementRef;
+  @ViewChild('roleText', { static: true }) roleText!: ElementRef;
+
+  readonly nameChars = 'Abdelrahman Hegab'.split('');
+  private tweens: { [key: string]: gsap.core.Tween } = {};
+
+  ngOnInit(): void {
+    // Initial state - hide texts
+    gsap.set([this.nameText.nativeElement, this.roleText.nativeElement], {
+      opacity: 0,
+      y: 50
+    });
+
+    const tl = gsap.timeline({ defaults: { ease: 'power3.out' } });
+    tl.to(this.nameText.nativeElement, {
+      opacity: 1,
+      y: 0,
+      duration: 1.5
+    })
+    .to(this.roleText.nativeElement, {
+      opacity: 1,
+      y: 0,
+      duration: 1.5
+    }, '-=1');
+
+    // Wait for initial animations to complete
+    setTimeout(() => {
+      // Setup bounce animations
+      const letters = gsap.utils.toArray<HTMLElement>('.char');
+
+      letters.forEach((letter, index) => {
+        // Create the tween
+        this.tweens[index.toString()] = gsap.to(letter, {
+          yPercent: -50,
+          duration: 0.4,
+          yoyo: true,
+          repeat: 1,
+          paused: true,
+          ease: "power2.out"
+        });
+
+        // Add data attribute
+        letter.dataset['tween'] = index.toString();
+
+        // Add direct hover listener
+        letter.addEventListener('mouseenter', () => {
+          const tween = this.tweens[letter.dataset['tween'] || ''];
+          if (tween && !gsap.isTweening(letter)) {
+            tween.restart();
+          }
+        });
+      });
+    }, 100);
+  }
 }
